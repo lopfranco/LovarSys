a1512f03967e10a920dab19257e30e77c1eeefee
Francisco Lopez Rodriguez <0floprod@afirme.com.mx>
2019-02-28 13:13:57 -0600

    Primer Commit

---
+++ app/src/main/java/com/example/ctc/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.ctc;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnEntrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
